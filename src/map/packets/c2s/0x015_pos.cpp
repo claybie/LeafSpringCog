a26ceee490b4512ad3fe29c71b25a6c61cbe061c
Clay The Realest <clay@claybie.org>
2025-08-11 17:52:44 -0400

    Mid-month merge with upstream

--- src/map/packets/c2s/0x015_pos.cpp
+++ src/map/packets/c2s/0x015_pos.cpp
@@ -33,11 +33,6 @@ auto GP_CLI_COMMAND_POS::validate(MapSession* PSession, const CCharEntity* PChar
 
 void GP_CLI_COMMAND_POS::process(MapSession* PSession, CCharEntity* PChar) const
 {
-    if (PChar->pendingPositionUpdate)
-    {
-        return;
-    }
-
     const float  newX        = x;
     const float  newY        = z; // Not a typo.
     const float  newZ        = y; // Not a typo.
@@ -62,7 +57,7 @@ void GP_CLI_COMMAND_POS::process(MapSession* PSession, CCharEntity* PChar) const
         PChar->loc.p.y = newY;
         PChar->loc.p.z = newZ;
 
-        PChar->loc.p.moving   = MoveFlame;
+        PChar->loc.p.moving   = MovTime;
         PChar->loc.p.rotation = newRotation;
 
         PChar->m_TargID = newTargID;
